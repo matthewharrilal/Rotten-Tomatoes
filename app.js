965ce6899d2a9d7a255cfe9098b1f4059e36c96f
Matthew Harrilal <matthewharrilal@gmail.com>
2018-08-30 20:52:47 -0700

    Views/Layouts added and installed templating engine handlebars

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express()
+var exphbs = require('express-handlebars')
+
+app.engine('handlebars', expbhs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
     res.send('Hello World')
 });
 
 app.listen(3000, () => {
-    console.log('Listening on port 3000') 
+    console.log('Listening on port 3000')
 });
